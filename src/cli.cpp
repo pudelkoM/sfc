ad68c0b6da8491df838919be667640b51979d24c
pudelkoM <maximilian.pudelko@gmail.com>
2019-01-27 16:49:01 +0100

    Add CLI parsing

---
+++ src/cli.cpp
@@ -0,0 +1,58 @@
+#include <CLI11.hpp>
+#include <string>
+#include <utility>
+
+struct args {
+    enum { ENCRYPT, DECRYPT } op;
+    std::string pw;
+};
+
+std::pair<struct args, int> parse_args(int argc, char *argv[]) {
+    CLI::App app{"Simple File Cryptor"};
+
+    // Supply password on the cli
+    std::string pw;
+    auto pw_flag_option = app.add_option("--pw", pw, "Read password from cli");
+
+    // Supply password as file
+    std::string pw_file;
+    auto pw_file_option =
+        app.add_option("--pw-file", pw_file, "Read password from file")->check(CLI::ExistingFile);
+
+    pw_file_option->excludes(pw_flag_option);
+    pw_flag_option->excludes(pw_file_option);
+
+    // Subcommands
+    app.require_subcommand();
+
+    auto enc_sub = app.add_subcommand("e", "Encrypt a file");
+    auto dec_sub = app.add_subcommand("d", "Decrypt a file");
+
+    // Expanded form of CLI11_PARSE(app, argc, argv);
+    try {
+        app.parse(argc, argv);
+    } catch (const CLI::ParseError &e) {
+        return {{}, app.exit(e)};
+    }
+
+    // Ensure we got either flag
+    if (app.count("--pw") == 0 && app.count("--pw-file") == 0) {
+        auto e = CLI::RequiredError("Either '--pw' or '--pw-file'");
+        return {{}, app.exit(e)};
+    }
+
+    // Read pw from file
+    if (app.count("--pw-file")) {
+        try {
+            pw.resize(4096);
+            auto c = std::ifstream(pw_file, std::ios::binary).read(pw.data(), pw.size()).gcount();
+            pw.resize(c);
+        } catch (std::ios::failure &f) {
+            auto e = CLI::FileError(pw_file);
+            return {{}, app.exit(e)};
+        }
+    }
+
+    struct args t = {enc_sub->parsed() ? args::ENCRYPT : args::DECRYPT, pw};
+    return {t, EXIT_SUCCESS};
+}
--- src/sfc.cpp
+++ src/sfc.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
 #include <array>
+#include <iostream>
+
 #include <hydrogen.h>
+
+// manual forward declarations for cli.cpp
+struct args {
+    enum { ENCRYPT, DECRYPT } op;
+    std::string pw;
+};
+std::pair<struct args, int> parse_args(int, char **);
 
 const char CONTEXT[hydro_pwhash_CONTEXTBYTES] = "sfc";
 const uint64_t OPSLIMIT = 10000;
@@ -13,64 +21,54 @@ const size_t PAYLOAD_SIZE = CHUNK_SIZE - hydro_secretbox_HEADERBYTES;
 
 using key_buffer = std::array<uint8_t, hydro_secretbox_KEYBYTES>;
 
-key_buffer generate_key(const std::string pw)
-{
+key_buffer generate_key(const std::string pw) {
     key_buffer derived_key;
-    hydro_pwhash_deterministic(derived_key.data(), derived_key.size(),
-                               pw.c_str(), pw.length(),
+    hydro_pwhash_deterministic(derived_key.data(), derived_key.size(), pw.c_str(), pw.length(),
                                CONTEXT, master_key, OPSLIMIT, MEMLIMIT, THREADS);
     return derived_key;
 }
 
-int check_stream(std::ios &stream)
-{
-    if (stream.eof())
-    {
+int check_stream(std::ios &stream) {
+    if (stream.eof()) {
         // done
         return EXIT_SUCCESS;
     }
-    if (stream.bad() || stream.fail())
-    {
+    if (stream.bad() || stream.fail()) {
         std::cerr << "io error" << std::endl;
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
 }
 
-int encrypt(std::istream &in, std::ostream &out, const key_buffer &key)
-{
+int encrypt(std::istream &in, std::ostream &out, const key_buffer &key) {
     std::array<uint8_t, PAYLOAD_SIZE> ibuffer;
     std::array<uint8_t, CHUNK_SIZE> obuffer;
     uint64_t chunk_nr = 0;
-    do
-    {
+    do {
         in.read(reinterpret_cast<char *>(ibuffer.data()), ibuffer.size());
         std::streamsize ilen = in.gcount();
-        hydro_secretbox_encrypt(obuffer.data(),
-                                ibuffer.data(), ilen,
-                                chunk_nr, CONTEXT, key.data());
+        hydro_secretbox_encrypt(obuffer.data(), ibuffer.data(), ilen, chunk_nr, CONTEXT,
+                                key.data());
         chunk_nr++;
         size_t olen = ilen + hydro_secretbox_HEADERBYTES;
         out.write(reinterpret_cast<char *>(obuffer.data()), olen);
     } while (in.good() && out.good());
 
+    in.sync();
+    out.flush();
     return check_stream(in) || check_stream(out);
 }
 
-int decrypt(std::istream &in, std::ostream &out, const key_buffer &key)
-{
+int decrypt(std::istream &in, std::ostream &out, const key_buffer &key) {
     std::array<uint8_t, CHUNK_SIZE> ibuffer;
     std::array<uint8_t, PAYLOAD_SIZE> obuffer;
     uint64_t chunk_nr = 0;
-    do
-    {
+    do {
         in.read(reinterpret_cast<char *>(ibuffer.data()), ibuffer.size());
         std::streamsize ilen = in.gcount();
-        int err = hydro_secretbox_decrypt(obuffer.data(),
-                                          ibuffer.data(), ilen,
-                                          chunk_nr, CONTEXT, key.data());
-        if (err)
-        {
+        int err = hydro_secretbox_decrypt(obuffer.data(), ibuffer.data(), ilen, chunk_nr, CONTEXT,
+                                          key.data());
+        if (err) {
             std::cerr << "decryption failed at block " << chunk_nr << std::endl;
             return err;
         }
@@ -79,28 +77,31 @@ int decrypt(std::istream &in, std::ostream &out, const key_buffer &key)
         out.write(reinterpret_cast<char *>(obuffer.data()), olen);
     } while (in.good() && out.good());
 
+    in.sync();
+    out.flush();
     return check_stream(in) || check_stream(out);
 }
 
-int main(int argc, char *argv[])
-{
-    if (hydro_init() != 0)
-    {
-        return EXIT_FAILURE;
-    }
-    if (argc != 2)
-    {
+int main(int argc, char *argv[]) {
+    if (hydro_init() != 0) {
+        std::cerr << "Failed to init hydro library" << std::endl;
         return EXIT_FAILURE;
     }
 
-    auto key = generate_key("hello");
+    struct args t;
+    int err;
+    std::tie(t, err) = parse_args(argc, argv);
+    if (err)
+        return err;
 
-    switch (*argv[1])
-    {
-    case 'e':
+    auto key = generate_key(t.pw);
+    hydro_memzero(t.pw.data(), t.pw.size());
+
+    switch (t.op) {
+    case args::ENCRYPT:
         return encrypt(std::cin, std::cout, key);
         break;
-    case 'd':
+    case args::DECRYPT:
         return decrypt(std::cin, std::cout, key);
         break;
     default:
